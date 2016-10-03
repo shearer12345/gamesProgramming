76d2c9883f2acfc3a6e313a2ce67fe36a6b55028
John Shearer <shearer12345@gmail.com>
2016-10-03 16:58:59 +0100

    workshop01 in-progress

---
+++ examples/buildSystem/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char* args[])
+{
+	std::cout << "HelloWorld!\n";
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init Error: %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_Log("SDL initialised OK!\n");
+	return 0;
+}
