7d2cf46443e44da695f91f44371f7d414ddf6fdc
John Shearer <shearer12345@gmail.com>
2016-10-04 08:56:06 +0100

    workshop 01 complete

--- examples/buildSystem/main.cpp
+++ examples/buildSystem/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "SDL.h"
 
-int main(int argc, char* args[])
+int main(int argc, char* argv[])
 {
-	std::cout << "HelloWorld!\n";
+	std::cout << "HelloWorld!\n"; //send HelloWorld! to console output, followed by a line break
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init Error: %s\n", SDL_GetError());
 		return 1;
 	}
 	SDL_Log("SDL initialised OK!\n");
+
 	return 0;
 }
