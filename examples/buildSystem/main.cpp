2975425814aeb6d2b835a625e5528cd80eecacd6
shearer12345 <shearer12345@gmail.com>
2016-10-05 10:32:33 +0100

    shorten lines in example

--- examples/buildSystem/main.cpp
+++ examples/buildSystem/main.cpp
@@ -3,10 +3,12 @@
 
 int main(int argc, char* argv[])
 {
-	std::cout << "HelloWorld!\n"; //send HelloWorld! to console output, followed by a line break
+	//send HelloWorld! to console output
+	std::cout << "HelloWorld!\n";
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init Error: %s\n", SDL_GetError());
+		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION,
+			"SDL_Init Error: %s\n", SDL_GetError());
 		return 1;
 	}
 	SDL_Log("SDL initialised OK!\n");
